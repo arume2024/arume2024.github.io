b93e0c4aab1f264f9272cf2296d8dd52b4c5a401
AcMaTeo <anhthong3010@gmail.com>
2024-02-20 07:57:11 +0700

    update code again

--- script/search-validation.js
+++ script/search-validation.js
@@ -1,5 +1,5 @@
 function gotoSearchResult() {
   let portValue = document.getElementById("port").value;
   portValue = portValue ? portValue : 10001;
-  window.location.href = `./Result.html?domain=${domain.value}&port=${portValue}`;
+  window.location.href = `./Result.html?searchKey=${domain.value}&port=${portValue}`;
 }
