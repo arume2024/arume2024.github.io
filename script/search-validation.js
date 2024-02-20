1c1127dfe88aea2fea148e452a3d6d236bc548d4
AcMaTeo <anhthong3010@gmail.com>
2024-02-20 07:29:55 +0700

    js for search button

--- script/search-validation.js
+++ script/search-validation.js
@@ -1,10 +1,4 @@
 function gotoSearchResult() {
-  const domain = document.getElementById("domain");
-  if (!domain.value) {
-    domain.classList.add("input-error");
-    return;
-  }
-
   let portValue = document.getElementById("port").value;
   portValue = portValue ? portValue : 10001;
   window.location.href = `./Result.html?domain=${domain.value}&port=${portValue}`;
